86b5b514358ab50f939f684f9460249290c0067a
Nimisha-1211 <84658758+Nimisha-1211@users.noreply.github.com>
2025-10-15 20:08:21 +0000

    added new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="nimi@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2 //not allowed
+accountEmail="hema@google.com"
+accountPassword="5678"
+accountCity="Bhopal"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
